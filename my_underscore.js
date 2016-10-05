1def986f53d0910caf595b847f21d5c94d14de60
flytiny <josondi@gmail.com>
2016-10-05 11:07:18 +0800

    version 1.2
    complete reduce &&  realize find

--- my_underscore.js
+++ my_underscore.js
@@ -14,38 +14,77 @@
     }
 
     __.each = __.foreach =  function(obj, fn){
-        var index = !isArrayLike(obj) && __.keys(obj);
-        var length = (index || obj).length;
+        var keys = !isArrayLike(obj) && __.keys(obj);
+        var length = (keys || obj).length;
         for(var i = 0; i < length; i++){
-            var currentKey = index ? index[i] : i;
+            var currentKey = keys ? keys[i] : i;
             fn(obj[currentKey], currentKey, obj);
         }
     };
     __.map = __.collect = function(obj, fn){
-        var index = !isArrayLike(obj) && __.keys(obj);
-        var length = (index || obj).length;
+        var keys = !isArrayLike(obj) && __.keys(obj);
+        var length = (keys || obj).length;
         var results = Array(length);
         for(var i = 0; i < length; i++){
-            var currentKey = index ? index[i] : i;
+            var currentKey = keys ? keys[i] : i;
             results[i] = fn(obj[currentKey], currentKey, obj);
         }
         return results;
     };
 
-    var createReduce = function(obj, fn){
-        var keys = !isArrayLike(obj) && __.keys(obj);
-        var length = (keys || obj).length;
-        var index = 0;
-        var results = obj[keys ? keys[index] : index];
-        index++;
-        for(; index >= 0 && index < length; index++){
-            var currentKey = keys ? keys[index] : index;
-            results = fn(results ,obj[currentKey], currentKey, obj);
+    var createReduce = function(dir){
+        var reducer = function(obj, fn, initial){
+            var keys = !isArrayLike(obj) && __.keys(obj);
+            var length = (keys || obj).length;
+            var index = dir > 0 ? 0 : length - 1;
+            if(!initial){
+                initial = obj[keys ? keys[index] : index];
+                index += dir;
+            }
+            for(; index >= 0 && index < length; index += dir){
+                var currentKey = keys ? keys[index] : index;
+                initial = fn(initial ,obj[currentKey], currentKey, obj);
+            }
+            return initial;
+        };
+        return function(obj, fn, initial){
+            return reducer(obj, fn, initial)
         }
-        return results;
     };
 
-    __.reduce = createReduce;
+    __.reduce  = __.foldl = __.inject = createReduce(1);
+    __.reduceRight = __.foldr = createReduce(-1);
+
+    __.find = function(obj, fn){
+        var keys = !isArrayLike(obj) && __.keys(obj);
+        var length = (keys || obj).length;
+        var results = Array(length);
+        for(var i = 0; i < length; i++){
+            var currentKey = keys ? keys[i] : i;
+            if(fn(obj[currentKey], currentKey, obj)){
+                return obj[currentKey];
+            }
+        }
+        return void 0;
+    };
+
+    //这两个方法限定了ArrayLike 这个版本的find没用到
+    __.findIndex = createPredicateIndexFinder(1);
+    __.findLastIndex = createPredicateIndexFinder(-1);
+
+    var createPredicateIndexFinder = function(dir){
+        var finder = function(array, fn){
+            var length = array['length'];
+            var index = dir > 0 ? 0 : length - 1;
+            for(; index >= 0 && index < length; index += dir){
+                if(fn(array[index], index, array)) return index;
+            }
+            return -1;
+        };
+        return function(array, fn){
+            return finder(array, fn);
+        }
+    };
 
     var isArrayLike = function(obj){
         var length = obj['length'];
