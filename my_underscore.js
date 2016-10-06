cb1cd16621a5823e5a7b0d0d02cf557160164516
flytinychen(陈俊杉) <flytinychen@tencent.com>
2016-10-06 11:25:28 +0800

    version 1.6
    complete pluck/invoke and realize max

--- my_underscore.js
+++ my_underscore.js
@@ -1,9 +1,9 @@
 /**
  * Created by flytinychen on 2016/10/4.
  */
-(function(){
+(function() {
     var root = this; //windows
-    var __ = function(){};
+    var __ = function() {};
     if (typeof exports != 'undefined' && !exports.nodeType) {
         if (typeof module != 'undefined' && !module.nodeType && module.exports) {
             exports = module.exports = __;
@@ -13,96 +13,134 @@
         root.__ = __;
     }
 
-    __.each = __.foreach =  function(obj, fn){
+    __.each = __.foreach = function(obj, fn) {
         var keys = !isArrayLike(obj) && __.keys(obj);
         var length = (keys || obj).length;
-        for(var i = 0; i < length; i++){
+        for (var i = 0; i < length; i++) {
             var currentKey = keys ? keys[i] : i;
             fn(obj[currentKey], currentKey, obj);
         }
     };
-    __.map = __.collect = function(obj, fn){
+    __.map = __.collect = function(obj, fn) {
         var keys = !isArrayLike(obj) && __.keys(obj);
         var length = (keys || obj).length;
         var results = Array(length);
-        for(var i = 0; i < length; i++){
+        for (var i = 0; i < length; i++) {
             var currentKey = keys ? keys[i] : i;
             results[i] = fn(obj[currentKey], currentKey, obj);
         }
         return results;
     };
 
-    var createReduce = function(dir){
-        var reducer = function(obj, fn, initial){
+    var createReduce = function(dir) {
+        var reducer = function(obj, fn, initial) {
             var keys = !isArrayLike(obj) && __.keys(obj);
             var length = (keys || obj).length;
             var index = dir > 0 ? 0 : length - 1;
-            if(!initial){
+            if (!initial) {
                 initial = obj[keys ? keys[index] : index];
                 index += dir;
             }
-            for(; index >= 0 && index < length; index += dir){
+            for (; index >= 0 && index < length; index += dir) {
                 var currentKey = keys ? keys[index] : index;
-                initial = fn(initial ,obj[currentKey], currentKey, obj);
+                initial = fn(initial, obj[currentKey], currentKey, obj);
             }
             return initial;
         };
-        return function(obj, fn, initial){
+        return function(obj, fn, initial) {
             return reducer(obj, fn, initial)
         }
     };
 
-    __.reduce  = __.foldl = __.inject = createReduce(1);
+    __.reduce = __.foldl = __.inject = createReduce(1);
     __.reduceRight = __.foldr = createReduce(-1);
 
-    __.find = function(obj, fn){
+    __.find = function(obj, fn) {
         var key = isArrayLike(obj) ? __.findIndex(obj, fn) : __.findKey(obj, fn);
-        if(key !== void 0 && key > -1) return obj[key];
+        if (key !== void 0 && key > -1) return obj[key];
     };
 
-    __.filter = function(obj, fn){
+    __.filter = function(obj, fn) {
         var results = [];
-        __.each(obj, function(value, index, list){
-            if(fn(value, index, list)) results.push(value);
+        __.each(obj, function(value, index, list) {
+            if (fn(value, index, list)) results.push(value);
         });
         return results;
     };
 
-    __.reject = function(obj, fn){
+    __.reject = function(obj, fn) {
         return __.filter(obj, __.negate(fn));
     };
 
-    __.every = function(obj, fn){
+    __.every = function(obj, fn) {
         var keys = !isArrayLike(obj) && __.keys(obj);
         var length = (keys || obj).length;
-        for(var i = 0; i < length; i++){
+        for (var i = 0; i < length; i++) {
             var currentKey = keys ? keys[i] : i;
-            if(!fn(obj[currentKey], currentKey, obj)) return false;
+            if (!fn(obj[currentKey], currentKey, obj)) return false;
         }
         return true;
     };
 
-    __.some = __.any = function(obj, fn){
+    __.some = __.any = function(obj, fn) {
         var keys = !isArrayLike(obj) && __.keys(obj);
         var length = (keys || obj).length;
-        for(var i = 0; i < length; i++){
+        for (var i = 0; i < length; i++) {
             var currentKey = keys ? keys[i] : i;
-            if(fn(obj[currentKey], currentKey, obj)) return true;
+            if (fn(obj[currentKey], currentKey, obj)) return true;
         }
         return false;
     };
 
-    __.contains = __.include = __.includes = function(obj, item, fromIndex){
-        if(!isArrayLike(obj)) obj = __.keys(obj);
-        if(typeof fromIndex != 'number') fromIndex = 0;
+    __.contains = __.include = __.includes = function(obj, item, fromIndex) {
+        if (!isArrayLike(obj)) obj = __.keys(obj);
+        if (typeof fromIndex != 'number') fromIndex = 0;
         return __.indexOf(obj, item, fromIndex) >= 0;
     };
 
-    __.where = function(obj, attr){
+    //something goes wrong
+    __.where = function(obj, attr) {
         return __.filter(obj, __.matcher(attr));
     };
 
-    __.matcher = __.matches = function(attrs){
+    __.findWhere = function(obj, attr) {
+        return _.find(obj, _.matcher(attrs));
+    };
+
+    __.pluck = function(obj, attrs) {
+        return __.map(obj, __.property(attrs));
+    };
+
+    __.invoke = function(obj, method) {
+        var isFunc = __.isFunction(method);
+        return __.map(obj, function(value) {
+            var func = isFunc ? method : value[method];
+            return func == null ? func : func.apply(value);
+        });
+    };
+
+    
+    __.max = function(obj, iteratee) {
+        var result;
+        var resultIndex
+
+        __.each(obj, function(value, index) {
+            if (index == 0) {
+                result = value;
+                resultIndex = 0;
+            }
+            var temp = __.isFunction(iteratee) ? iteratee(value, index, obj) : value;
+            var resultTemp = __.isFunction(iteratee) ? iteratee(result, resultIndex, obj) : result;
+
+            if (temp > resultTemp) result = value;
+        })
+        return result;
+    }
+
+
+
+
+    __.matcher = __.matches = function(attrs) {
         attrs = __.extendOwn({}, attrs);
         return function(obj) {
             return __.isMatch(obj, attrs);
@@ -110,7 +148,7 @@
     };
 
     // 无法解决{c:2} == {c:2}为false的问题 待优化
-    __.isMatch = function(object, attrs){
+    __.isMatch = function(object, attrs) {
         var keys = __.keys(attrs);
         var length = keys.length;
         if (object == null) return !length;
@@ -142,23 +180,23 @@
     __.extend = createAssigner(__.allKeys);
     __.extendOwn = __.assign = createAssigner(__.keys);
 
-    __.negate = function(fn){
-        return function(){
-            return !fn.apply(this,arguments);
+    __.negate = function(fn) {
+        return function() {
+            return !fn.apply(this, arguments);
         }
     };
 
     //这两个方法限定了ArrayLike
-    var createPredicateIndexFinder = function(dir){
-        var finder = function(array, fn){
+    var createPredicateIndexFinder = function(dir) {
+        var finder = function(array, fn) {
             var length = array['length'];
             var index = dir > 0 ? 0 : length - 1;
-            for(; index >= 0 && index < length; index += dir){
-                if(fn(array[index], index, array)) return index;
+            for (; index >= 0 && index < length; index += dir) {
+                if (fn(array[index], index, array)) return index;
             }
             return -1;
         };
-        return function(array, fn){
+        return function(array, fn) {
             return finder(array, fn);
         }
     };
@@ -166,30 +204,39 @@
     __.findIndex = createPredicateIndexFinder(1);
     __.findLastIndex = createPredicateIndexFinder(-1);
 
-    __.findKey = function(obj, fn){
+    __.findKey = function(obj, fn) {
         var keys = __.keys(obj);
-        var length  = keys.length;
-        for(var index = 0; index < length; index++){
-            if(fn(obj[keys[index]], keys[index], obj)) return keys[index];
+        var length = keys.length;
+        for (var index = 0; index < length; index++) {
+            if (fn(obj[keys[index]], keys[index], obj)) return keys[index];
         }
     };
 
-    var isArrayLike = function(obj){
+    var property = function(key) {
+        return function(obj) {
+            return obj == null ? void 0 : obj[key];
+        };
+    };
+
+    __.property = property;
+
+    // __.restArgs = restArgs;
+    var isArrayLike = function(obj) {
         var length = obj['length'];
         return length && typeof length == 'number' && length >= 0;
     };
 
-    __.keys = function(obj){
-        if(!__.isObject(obj)) return [];
-        if(Object.keys) return Object.keys(obj);
+    __.keys = function(obj) {
+        if (!__.isObject(obj)) return [];
+        if (Object.keys) return Object.keys(obj);
         var results = [];
-        for(var index in obj){
+        for (var index in obj) {
             if (__.has(obj, key)) keys.push(key);
         }
         return results;
     };
 
-    __.values = function(obj){
+    __.values = function(obj) {
         var keys = __.keys(obj);
         var length = keys.length;
         var values = Array(length);
@@ -199,17 +246,29 @@
         return values;
     };
 
-    __.allKeys = function(obj){
-        if(__.isObject(obj)) return [];
-        if(object.keys) return object.keys(obj);
+    __.allKeys = function(obj) {
+        if (__.isObject(obj)) return [];
+        if (object.keys) return object.keys(obj);
         var results = [];
-        for(var index in obj){
+        for (var index in obj) {
             results.push(index);
         }
         return results;
     };
 
-    __.has = function(obj, keys){
+    __.isFunction = function(obj) {
+        return typeof obj == 'function' || false;
+    };
+
+    __.isUndefined = function(obj) {
+        return obj === void 0;
+    };
+
+    __.isBoolean = function(obj) {
+        return obj === true || obj === false || toString.call(obj) === '[object Boolean]';
+    };
+
+    __.has = function(obj, keys) {
         return obj != null && obj.hasOwnProperty(keys);
     };
 
